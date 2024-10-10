cd7b6d2ec966122945988c166ebbe030e5d9f739
Divij Sharma <divijs75@gmail.com>
2024-10-10 15:30:58 +0530

    feat(server): Configure the server settings

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cron from 'node-cron';
+import { fetchCryptoData } from './jobs/fetchCryptoData';
+import statsRoutes from './routes/statsRoutes';
+import deviationRoutes from './routes/deviationRoutes';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => console.log('MongoDB client connected'))
+  .catch((error) => console.log('Error connecting to MongoDB client:', error));
+
+const temp = async () => {
+  console.log(`Fetching crypto data at machine time ${new Date().toISOString()}`);
+  await fetchCryptoData();
+}
+temp();
+// cron.schedule('0 */2 * * *', async () => {
+//   console.log('Fetching crypto data...');
+//   await fetchCryptoData();
+// });
+
+app.use(express.json());
+
+app.use('/api/v1', statsRoutes);
+app.use('/api/v1', deviationRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is up and burning on localhost:${PORT} 🔥`);
+});
